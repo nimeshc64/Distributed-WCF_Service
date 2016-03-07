f6cc4840f270b3a213563b28bd764bdf48b3cbfe
nimeshc64 <nimeshc64@gmail.com>
2016-03-07 23:56:20 +0530

    comment change

--- DisasterServices/DisasterServices/DisasterService.svc.cs
+++ DisasterServices/DisasterServices/DisasterService.svc.cs
@@ -7,8 +7,7 @@ using System.Text;
 
 namespace DisasterServices
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DisasterService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select DisasterService.svc or DisasterService.svc.cs at the Solution Explorer and start debugging.
+    
     public class DisasterService : IDisasterService, IAreaService,IDisasterTypeService,ISuggestion,Iuser,IUserAlerts
     {
         #region area
--- DisasterServices/DisasterServices/IDisasterService.cs
+++ DisasterServices/DisasterServices/IDisasterService.cs
@@ -8,7 +8,6 @@ using System.Text;
 
 namespace DisasterServices
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDisasterService" in both code and config file together.
     [ServiceContract]
     public interface IDisasterService
     {
